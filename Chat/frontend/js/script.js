8bf5a8fb7effe16b1787490d4d5428054ff8b58c
pazolini24 <112673444+pazolini24@users.noreply.github.com>
2024-01-11 10:40:06 -0300

    Update script.js
    conectando link do servidor pelo render

--- Chat/frontend/js/script.js
+++ Chat/frontend/js/script.js
@@ -84,7 +84,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://chat-backend-8nx7.onrender.com")
     websocket.onmessage = processMessage
 
 }
